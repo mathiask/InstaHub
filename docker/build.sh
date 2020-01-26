04c5edf03851fb52bbcc51b56f4973e4e63267ed
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2020-01-26 17:30:36 +0000

    added comments/echo

--- docker/build.sh
+++ docker/build.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
+# cd to the directory where the script is
 cd "$(dirname "$0")"
 
+echo -e "\nBuilding DB image..."
 docker build -t instahubdb .
 
 cd ..
+echo -e "\nCreating .env from .env.example..."
 sed -r -e 's/^DB_HOST=.*/DB_HOST=db/'\
        -e 's/^(DB_(DATABASE|USERNAME|PASSWORD))=.*/\1=instahub/'\
   .env.example\
   >>.env
 
+echo -e "\nRunning composer (in a docker container)..."
 docker run --rm --volume $PWD:/app composer install
