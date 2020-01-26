7c31c11e34329b8cf886261fee33ff5d023f49d8
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2020-01-26 17:25:01 +0000

    generate .env file

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
+
 cd "$(dirname "$0")"
+
 docker build -t instahubdb .
+
+cd ..
+sed -r -e 's/^DB_HOST=.*/DB_HOST=db/'\
+       -e 's/^(DB_(DATABASE|USERNAME|PASSWORD))=.*/\1=instahub/'\
+  .env.example\
+  >>.env
